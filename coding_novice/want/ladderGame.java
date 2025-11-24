6ec92169be6f6bac848657edd51e0129b8b07bc1
jjb <jubin030419@gmail.com>
2025-11-24 12:05:22 +0900

    A

---
+++ coding_novice/want/ladderGame.java
@@ -0,0 +1,19 @@
+package want;
+import java.util.Scanner;
+
+public class ladderGame {
+    /*사다리게임, 생각보다 스케일이 커서 방치하며 차차 해봄
+    총 인원은 4명으로 하며 같은 1:1 배치
+    array 사용하며 한 줄씩 내려가는 느낌
+    0 0 0 0
+    0 0 0 0
+    0 0 0 0
+    0 0 0 0 
+     */
+    public static void main(String[] args) {
+        //변수 정의, person(참가자) / ladder(사다리) / answer(결과)
+        
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
