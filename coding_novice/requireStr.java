2d1f6eaab40afc443ce67025e31a151a5950cd30
jjb <jubin030419@gmail.com>
2025-11-28 13:12:11 +0900

    .equals만 알면 쉬운편

--- coding_novice/requireStr.java
+++ coding_novice/requireStr.java
@@ -8,12 +8,28 @@ public class requireStr {
     "<", "!" : n < m
     두 문자열 ineq와 eq가 주어집니다. ineq는 "<"와 ">"중 하나고, 
     eq는 "="와 "!"중 하나입니다. 그리고 두 정수 n과 m이 주어질 때, 
-    n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록 solution 함수를 완성해주세요.
-
-
+    n과 m이 ineq와 eq의 조건에 맞으면 1을 아니면 0을 return하도록
      */
     public int solution(String ineq, String eq, int n, int m) {
         int answer = 0;
+        //ineq = < or >  / eq = "=" or !
+        //👩🏼‍💻 .equals() 사용하기!
+        if (ineq.equals(">") && eq.equals("="))
+            return n>=m ? 1:0;
+        if (ineq.equals("<") && eq.equals("="))
+            return n<=m ? 1:0;
+        if (ineq.equals(">") && eq.equals("!"))
+            return n>m ? 1:0;
+        if (ineq.equals("<") && eq.equals("!"))
+            return n<m ? 1:0;
+
+
         return answer;
     }
 }
+
+/*      실패한 코드 .equals 사용해야힘.
+        if (ineq == "<" && eq == "=" ? n<=m : n>=m);
+        if (ineq == ">" && eq == "=" ? n>=m : n<=m);
+        if (ineq == "<" && eq == "!" ? n>m : n<m);
+        if (ineq == ">" && eq == "!" ? n<m : n>m); */
