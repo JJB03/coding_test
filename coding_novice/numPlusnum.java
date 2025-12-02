568cd9bec89a2e140aa3781c865a2043918927f2
jjb <jubin030419@gmail.com>
2025-12-02 15:32:44 +0900

    한 번 싹 다시 해보고 따로 공부할 부분 적는 게 좋나

--- coding_novice/numPlusnum.java
+++ coding_novice/numPlusnum.java
@@ -6,8 +6,17 @@ public class numPlusnum {
     = 홀수 이어붙인 수 + 짝수 이어 붙인 수 = 합
      */
         public int solution(int[] num_list) {
-        int answer = 0;
-
-        return answer;
+            String even = "";
+            String odd = "";
+            //홀수 값 / 짝수 값 분리하기
+            for (int i = 0; i < num_list.length; i++) { 
+                if (num_list[i] % 2 == 0) {// 짝수라면 + 배열이니까 이런 식으로 넣어야 함 기억.
+                    even += Integer.toString(num_list[i]);
+                } else {
+                    odd += Integer.toString(num_list[i]);
+                }
+            }
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+        //아 valueOf는 String으로 변하게 하는 거임 잘못 판단
     }
 }
