7b3f5561c93b2699535d381904f451f90026b8cb
jjb <jubin030419@gmail.com>
2025-12-07 19:57:51 +0900

    update

---
+++ coding_novice/arr_change.java
@@ -0,0 +1,18 @@
+public class arr_change {
+/*
+정수 배열 arr와 2차원 정수 배열 queries이 주어집니다. 
+queries의 원소는 각각 하나의 query를 나타내며, [i, j] 꼴입니다.
+각 query마다 순서대로 arr[i]의 값과 arr[j]의 값을 서로 바꿉니다.
+위 규칙에 따라 queries를 처리한 이후의 arr를 return 
+  */   
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = {};
+        //버블?교환하라인가 이게
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                
+            }
+        }
+        return answer;
+    }
+}
