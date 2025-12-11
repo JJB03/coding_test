8d67e9b21f496927ff0cc63b9f0eb30643be2ce7
jjb <jubin030419@gmail.com>
2025-12-11 22:16:05 +0900

    일단 분석해보기

--- coding_novice/retry/point_retry/arr_maker.java
+++ coding_novice/retry/point_retry/arr_maker.java
@@ -9,16 +9,15 @@ public class arr_maker {
     모든 정수를 오름차순으로 저장한 배열을 return 하는 solution 함수를 완성해 주세요.
     만약 그러한 정수가 없다면, -1이 담긴 배열을 return 합니다.
      */
-    public int[] solution(int l, int r) {
+public int[] solution(int l, int r) {
         List<Integer> list = new ArrayList<>();
-        //반복 검사해야 해서 for문 필수
-        //코파일럿 사용.
-        for (int i = l; i <=r; i++) {
-            String s = String.valueOf(i); //문자열로 변경해서 정확하게 확인
+
+        for (int i = l; i <= r; i++) {
+            String s = String.valueOf(i);
             boolean valid = true;
 
             for (char c : s.toCharArray()) {
-                if (c != 0 && c != 5 ) {
+                if (c != '0' && c != '5') {  // 문자 비교로 수정
                     valid = false;
                     break;
                 }
@@ -32,7 +31,7 @@ public class arr_maker {
         if (list.isEmpty()) {
             return new int[]{-1};
         }
-        // list -> int[] 로 변환
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
