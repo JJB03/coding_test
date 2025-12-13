207764fd7ec59e1f5b1c93b4f3e6aa139c4cb75f
jjb <jubin030419@gmail.com>
2025-12-13 23:45:20 +0900

    list 배우는 중

--- coding_novice/count_up.java
+++ coding_novice/count_up.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class count_up {
     /*
     정수 start_num와 end_num가 주어질 때
     start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
      */
     public int[] solution(int start_num, int end_num) {
-        int[] answer = {};
+        List<Integer> answer = new ArrayList<>();
+        //리스트는 비연속적인 배열임.
         return answer;
     }
 }
