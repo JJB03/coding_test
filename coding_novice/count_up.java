abc51393f12c0783f46b6fd6557bb569cb19774d
jjb <jubin030419@gmail.com>
2025-12-13 23:58:06 +0900

    도움을 좀 받음. 유용한듯

--- coding_novice/count_up.java
+++ coding_novice/count_up.java
@@ -6,9 +6,13 @@ public class count_up {
     정수 start_num와 end_num가 주어질 때
     start_num부터 end_num까지의 숫자를 차례로 담은 리스트를 return하도록 solution 함수를 완성해주세요.
      */
-    public int[] solution(int start_num, int end_num) {
-        List<Integer> answer = new ArrayList<>();
+    public List<Integer> solution(int start_num, int end_num) {
+        List<Integer> answer = new ArrayList<Integer>();
         //리스트는 비연속적인 배열임.
+        for (int i = start_num; i <= end_num; i++) {
+            //시작부터 끝까지
+            answer.add(i);
+        }
         return answer;
     }
 }
