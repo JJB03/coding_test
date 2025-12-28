af39e8753aa62a66b7a64595e1670452224eb0aa
jjb <jubin030419@gmail.com>
2025-12-28 23:48:45 +0900

    code 해설 볼 필요 O

--- coding_novice/retry/arr5.java
+++ coding_novice/retry/arr5.java
@@ -1,3 +1,8 @@
+package retry;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class arr5 {
 /*
 문자열 배열 intStrs와 정수 k, s, l가 주어집니다. 
@@ -5,8 +10,15 @@ intStrs의 원소는 숫자로 이루어져 있습니다.
 배열 intStrs의 각 원소마다 s번 인덱스에서 시작하는 길이 l짜리 부분 문자열을 잘라내 정수로 변환합니다. 
 이때 변환한 정수값이 k보다 큰 값들을 담은 배열을 return 하는 solution 함수를 완성해 주세요.
  */
-    public int[] solution(String[] intStrs, int k, int s, int l) {
-        int[] answer = {};
+    public List<Integer> solution(String[] intStrs, int k, int s, int l) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for (int i = 0; i < intStrs.length; i++) {
+            int newInt = Integer.parseInt(intStrs[i].substring(s, s + l));
+            if (newInt > k) {
+                answer.add(newInt);
+            }
+        }
         return answer;
     }
 }
