f66934627f28b9641c4210db2870f05ea2d894f8
jjb <jubin030419@gmail.com>
2026-01-04 23:07:48 +0900

    new test

---
+++ coding_novice/string_add_n.java
@@ -0,0 +1,10 @@
+public class string_add_n {
+    /*
+    문자열 my_string과 정수 n이 매개변수로 주어질 때, 
+    my_string의 뒤의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+     */
+    public String solution(String my_string, int n) {
+        String answer = "";
+        return answer;
+    }
+}
