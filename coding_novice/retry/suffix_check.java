f6f3d7ab23978549cfa9901249c3bb1a8cb08c68
jjb <jubin030419@gmail.com>
2026-01-06 22:37:14 +0900

    그렇게 어렵진 않음, 재복습 필요 노션에 추가하기.

--- coding_novice/retry/suffix_check.java
+++ coding_novice/retry/suffix_check.java
@@ -1,3 +1,4 @@
+package retry;
 public class suffix_check {
 /*
 어떤 문자열에 대해서 접미사는 특정 인덱스부터 시작하는 문자열을 의미합니다. 
@@ -7,7 +8,13 @@ public class suffix_check {
 is_suffix가 my_string의 접미사라면 1을, 아니면 0을 return 하는 solution 함수를 작성해 주세요.
  */
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        return answer;
+        //그니까 즉, 음절 모두 띄우는 게 아닌 위에서 하나씩 -
+            //my_string == is_suffix가 같다면 1
+            //다만 문자열 ==는 주소를 비교하는 것이다.
+            if (is_suffix.length() <= my_string.length()
+            && my_string.substring(my_string.length() - is_suffix.length()).equals(is_suffix)) {
+                return 1;
+            } return 0;
+    
     }
 }
