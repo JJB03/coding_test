daa7b8231ea9cb6edc1595840a3f4ce131317de7
jjb <jubin030419@gmail.com>
2026-01-07 21:31:17 +0900

    newtest

---
+++ coding_novice/n_string.java
@@ -0,0 +1,14 @@
+public class n_string {
+/*
+문자열 my_string과 정수 n이 매개변수로 주어질 때, 
+my_string의 앞의 n글자로 이루어진 문자열을 return 하는 solution 함수를 작성해 주세요.
+my_string	        n	result
+"ProgrammerS123"	11	"ProgrammerS"
+ */
+    public String solution(String my_string, int n) {
+        String answer = "";
+        //그러니까 자르고 앞에 문장 꺼내오기.
+        //for문이 필요할까?
+        return answer;
+    }
+}
