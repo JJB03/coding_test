871a375b35b054b741a03eea72d45c617ade13f7
jjb <jubin030419@gmail.com>
2026-01-19 20:52:41 +0900

    다시 파악하기

--- coding_novice/retry/point_retry/vertical_string.java
+++ coding_novice/retry/point_retry/vertical_string.java
@@ -7,10 +7,12 @@ public class vertical_string {
 my_string을 한 줄에 m 글자씩 가로로 적었을 때
 왼쪽부터 세로로 c번째 열에 적힌 글자들을 문자열로 return 하는 solution 함수를 작성해 주세요.
  */
-//풀이가 생각보다 쉬운데 내가 잘못 생각하고 있는게 있나.
+//저장할 필요가 없어서 List 소환 불필요. idx 개념을 잘 아는게 중요.
     public String solution(String my_string, int m, int c) {
         StringBuilder sb = new StringBuilder();
         int idx = my_string.length();
+        //0부터 시작하니 실제로는 c - 1 위치.
+        //- i += m : 한 줄에 m글자씩 있으니까, 같은 열의 다음 글자는 항상 m만큼 떨어져 있습니다.
         for(int i = c - 1; i < idx; i += m) {
             sb.append(my_string.charAt(i));
         }
