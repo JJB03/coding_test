fd0539409e8cfbfb6c89c4b6c940bbb03849f2e3
jjb <jubin030419@gmail.com>
2026-01-30 03:32:35 +0900

    어렵군!

--- coding_novice/conut_string.java
+++ coding_novice/conut_string.java
@@ -1,9 +1,19 @@
 public class conut_string {
 /*
-알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를 순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
+알파벳 대소문자로만 이루어진 문자열 my_string이 주어질 때, 
+my_string에서 'A'의 개수, my_string에서 'B'의 개수,..., my_string에서 'Z'의 개수, 
+my_string에서 'a'의 개수, my_string에서 'b'의 개수,..., my_string에서 'z'의 개수를 
+순서대로 담은 길이 52의 정수 배열을 return 하는 solution 함수를 작성해 주세요.
  */
     public int[] solution(String my_string) {
-        int[] answer = {};
+        int[] answer = {};//여기에 담음
+        //대소문자를 따로 분리해서 따로 세기?
+        //일단 세는 거 만들자
+        for (int i = 0; i < my_string.length(); i++) {
+            if () {
+                
+            }
+        }
         return answer;
     }
 }
